3fd0689961763a8f70ef81c14ef8c55531719020
sk25 <sk25@2e5b0437-9272-43a4-8809-2444bc04dd50>
2015-08-14 21:52:19 +0000

    Version 0.1 - 2015-08-14 - First release

---
+++ tags/v.0.1/WinHosts Manager/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WinHosts_Manager
+{
+	public class Configuration
+	{
+		public Configuration()
+		{
+			Hosts = new List<WinHost>();
+		}
+
+		public List<WinHost> Hosts { get; set; }
+	}
+}
---
+++ tags/v.0.1/WinHosts Manager/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WinHosts_Manager
+{
+	/// <summary>
+	/// Logica di interazione per MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private Data AppData
+		{
+			get { return (App.Current as App).Data; }
+		}
+
+		private void btnWriteToHosts_Click(object sender, RoutedEventArgs e)
+		{
+			(App.Current as App).WriteToWinHosts();
+			(App.Current as App).SaveConfiguration();
+			MessageBox.Show("Configuration written to hosts");
+		}
+
+		private void btnNewHost_Click(object sender, RoutedEventArgs e)
+		{
+			AppData.Hosts.Add(new WinHost());
+		}
+
+		private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (e.AddedItems.Count > 0)
+			{
+				wHostProperties.Subject = e.AddedItems[0] as WinHost;
+			}
+		}
+	}
+}
