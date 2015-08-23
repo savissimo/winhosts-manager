b2f7bdda25fb0357b273c8d97f2611b884f705bd
sk25 <sk25@2e5b0437-9272-43a4-8809-2444bc04dd50>
2015-08-23 23:05:22 +0000

    Version 0.2 - 2015-08-24 - Added "Remove host" - Added icon set

---
+++ tags/v.0.2/WinHosts Manager/Data.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace WinHosts_Manager
+{
+	public class Data
+	{
+		private ObservableCollection<WinHost> m_listHosts = new ObservableCollection<WinHost>();
+		public ObservableCollection<WinHost> Hosts
+		{
+			get { return m_listHosts; }
+			set { m_listHosts = value; }
+		}
+	}
+}
---
+++ tags/v.0.2/WinHosts Manager/WinHostProperties.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WinHosts_Manager
+{
+	/// <summary>
+	/// Logica di interazione per WinHostProperties.xaml
+	/// </summary>
+	public partial class WinHostPropertiesWidget : UserControl
+	{
+		public WinHost Subject
+		{
+			get { return (WinHost)GetValue(SubjectProperty); }
+			set { SetValue(SubjectProperty, value); }
+		}
+
+		// Using a DependencyProperty as the backing store for Subject.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty SubjectProperty = DependencyProperty.Register(
+				"Subject", typeof(WinHost), typeof(WinHostPropertiesWidget), new UIPropertyMetadata(null));		
+
+		public WinHostPropertiesWidget()
+		{
+			InitializeComponent();
+		}
+	}
+}
